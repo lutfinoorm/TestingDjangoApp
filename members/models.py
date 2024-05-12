e5003a7b30a132257c47426bede8594bf338935f
lutfinoor muhamad <lutfinoor.muhamad@gmail.com>
2024-05-12 10:03:26 +0700

    edit admin menu

--- members/models.py
+++ members/models.py
@@ -4,3 +4,6 @@ class Member(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
   email = models.EmailField(max_length=255, null=True)
+
+  def __str__(self):
+    return f"({self.id}) {self.firstname} {self.lastname}"
